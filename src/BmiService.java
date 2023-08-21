47be228202c3ebbb3644b41c5f889d7a27a419cc
BrevnEl <lenadmitrieva2014@yandex.ru>
2023-08-21 16:44:06 +0300

    2

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService
+{
+    public int calculate(double heightMetres, int weightKg)
+    {
+        double heightS = heightMetres *  heightMetres;
+        double myIndex = weightKg/ heightS;
+        int result = (int) myIndex;
+        return result;
+    }
+}
+
